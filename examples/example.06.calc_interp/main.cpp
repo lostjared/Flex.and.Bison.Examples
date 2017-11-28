e19ba7ad6e66e6e0ffad94a6e23f458c11c130e9
lostjared <jaredbruni@gmail.com>
2017-11-28 10:27:42 -0800

    updated removed if

--- examples/example.06.calc_interp/main.cpp
+++ examples/example.06.calc_interp/main.cpp
@@ -2,12 +2,9 @@
 #include "astheader.hpp"
 
 extern int yyparse();
-int arg_count = 0;
+
 int main(int argc, char **argv) {
-    arg_count = argc;
-    if(argc == 1) {
-    	std::cout << "$> ";
-    }
+   	std::cout << "$> ";
     yyparse();
     return 0;
 }
