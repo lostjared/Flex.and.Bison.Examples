f3af85b9a40aa073cbb5f1cf1b8bfb2e185278b1
lostjared <jaredbruni@gmail.com>
2017-12-17 12:36:20 -0800

    added example Makefile/source file

---
+++ examples/example.08.tree/README.md
@@ -0,0 +1,17 @@
+# Abstract Syntax Tree 
+
+To use this program first type
+
+$ make flex
+
+Then 
+
+$ make
+
+then to compile example source file .exp
+
+$ make -f Makefile.test
+
+then you can run the demo or checkout output.c
+
+
--- examples/example.08.tree/main.cpp
+++ examples/example.08.tree/main.cpp
@@ -1,15 +1,27 @@
 #include"tree.hpp"
+#include<cstdio>
 
 extern int yyparse();
+extern void yyrestart(FILE *fptr);
 
 std::unordered_map<std::string, Symbol> symbols;
 std::ostringstream code_stream;
 std::ostringstream var_stream;
 StringTree ast;
 
-int main() {
-    std::cout << "$> ";
-    yyparse();
+int main(int argc, char **argv) {
+    if(argc == 1) {
+    	std::cout << "$> ";
+    	yyparse();
+    } else if(argc == 2) {
+        FILE *fptr = fopen(argv[1], "r");
+        yyrestart(fptr);
+        yyparse();
+        fclose(fptr);
+        
+    } else {
+        std::cerr << "invalid arguments..\n";
+    }
 	return 0;
 }
 
