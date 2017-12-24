012618882f2c527078f3a009c2dedb1553fd697f
lostjared <jaredbruni@gmail.com>
2017-12-23 16:56:11 -0800

    updated string variable declaration/assignment

--- examples/example.08.tree/tree.hpp
+++ examples/example.08.tree/tree.hpp
@@ -170,7 +170,14 @@ public:
         fobj << "\n\n";
         fobj << "int main() {\n";
         for(auto i = symbols.begin(); i != symbols.end(); ++i) {
-            var_stream << "double " << i->first << " = 0;\n";
+            switch(i->second.vtype) {
+                case Var_id::ID_NUMERIC:
+                    var_stream << "double " << i->first << " = 0;\n";
+                    break;
+                case Var_id::ID_STRING:
+                    var_stream << "const char *sz_" << i->first << " = 0;\n";
+                    break;
+            }
         }
         fobj << var_stream.str();
         fobj << code_stream.str();
@@ -215,7 +222,7 @@ public:
             case Var_type::S_EQUAL: {
                 symbols[node->sym->name].str_value = node->left->sym->str_value;
                 symbols[node->sym->name].vtype = Var_id::ID_STRING;
-                code_stream << "const char *sz_" << node->sym->name << " = " << node->left->sym->str_value << ";\n";
+                code_stream << "sz_" << node->sym->name << " = " << node->left->sym->str_value << ";\n";
                 return 0;
             }
                 break;
