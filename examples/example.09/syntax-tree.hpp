4a7c11aedb53c9df0332ff7adb9e5c8f3af7cd0e
lostjared <jaredbruni@gmail.com>
2018-01-09 17:23:24 -0800

    add AST_Node functions

--- examples/example.09/syntax-tree.hpp
+++ examples/example.09/syntax-tree.hpp
@@ -27,13 +27,28 @@ namespace ast {
     Symbol *createSymbol(std::string value);
     Symbol *createSymbol(double d);
     Symbol *createSymbol(std::string n, double v);
-    
+
+    template<typename T>
     class AST_Node {
     public:
+        T type;
         Symbol *sym;
+        unsigned int node_type;
         AST_Node *left, *right;
+        
+        AST_Node() : type(T()), sym(0), node_type(0), left(0), right(0) {}
+        AST_Node(T ntype, unsigned int nnode_type, Symbol *s, AST_Node *nleft, AST_Node *nright) : type(type), node_type(nnode_type), sym(s), left(nleft), right(nright) {}
     };
     
+    template<typename T>
+    AST_Node<T> *createNode(T type, unsigned int node_type, Symbol *s, AST_Node<T> *left, AST_Node<T> *right) {
+        AST_Node<T> *n = new AST_Node<T>(type,node_type,s,left,right);
+        if(!n) {
+            std::cerr << "Error could not create node..\n";
+            return nullptr;
+        }
+        return n;
+    }
 }
 
 extern void yyerror(const char *str, ...);
